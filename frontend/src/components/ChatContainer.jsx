c12fd6c578b0c26ef57975033c2258a2b803deef
Kidus Petros <kidusptr@gmail.com>
2025-05-16 20:53:27 +0300

    working on chat page

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const ChatContainer = () => {
-  return (
-    <div>
-      <h1>ChatContainer</h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default ChatContainer;
