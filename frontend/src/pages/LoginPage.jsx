91de6e3209de47cf9503c5f70be70defe5722556
Kidus Petros <kidusptr@gmail.com>
2025-05-17 22:44:51 +0300

    working on sign up page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import assets from "../assets/assets";
 
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenlt max-sm:flex-col backdrop-blur-2xl">
+      {/* Background image */}
+      <img src={assets.logo_big} alt="" className="w-[min(30vw,250px)]" />
+      {/* Login form */}
+      <form
+        action=""
+        className="border-2 bg-white/2 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg"
+      >
+        <h2></h2>
+      </form>
     </div>
   );
 };
