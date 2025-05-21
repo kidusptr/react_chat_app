1d8d1b1dcf1ffca58e70df3b467900c89557a8f7
Kidus Petros <kidusptr@gmail.com>
2025-05-21 22:06:39 +0300

    working on profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const ProfilePage = () => {
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
+      <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex  items-center jsutidy-between max-sm:flex-col-reverse rounded-lg">
+        <form action="" className="flex flex-col gap-5  p-10 flex-1">
+          <h3 className="text-lg">Profile details</h3>
+        </form>
+        <img src="" alt="" />
+      </div>
     </div>
   );
 };
